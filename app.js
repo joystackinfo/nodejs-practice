36f01291678a3e606a3e69e3d17f545b9ad71ae0
joystackinfo <joynelsoninfo@gmail.com>
2025-08-16 21:49:34 +0100

    Updated writeable stream

--- app.js
+++ app.js
@@ -1,77 +1,87 @@
-const fs=require('fs')
-const http=require('http')
-const path = require('path')
-const os=require('os')
-const url =require('url')
+ const fs=require('fs')
+ const http=require('http')
+ const path = require('path')
+ const os=require('os')
+ const url =require('url')
 const crypto=require('crypto')
-const sayHello = require ('./greetings')
-const math=require('./math')
-const lodash= require('lodash');
+ const sayHello = require ('./greetings')
+ const math=require('./math')
+ const lodash= require('lodash');
 
-//fs.readFile('example.txt', 'utf8', (err, data) => {
-//  if (err) {
-//      console.error(err)
-//      return;
-//  }
-//  console.log(data)
-//});
+// //fs.readFile('example.txt', 'utf8', (err, data) => {
+// //  if (err) {
+// //      console.error(err)
+// //      return;
+// //  }
+// //  console.log(data)
+// //});
 
-///fs.writeFile('output.txt', 'Hello, NodeJs', (err) => {
-//    if (err) {
-//        console.error(err);
-//        return;
-//    }
-//    console.log('File written successfully');
-//});
+// ///fs.writeFile('output.txt', 'Hello, NodeJs', (err) => {
+// //    if (err) {
+// //        console.error(err);
+// //        return;
+// //    }
+// //    console.log('File written successfully');
+// //});
 
-//const server=http.createServer((req,res) => {
-    //res.statusCode=200
-    //res.setHeader('content-type' , 'text/plain')
-    //res.end('HELLO , NODE JS')
-//});
-//server.listen(3000,() => {
-    //console.log('Server running at http://localhost:3000/');
-//});
+// //const server=http.createServer((req,res) => {
+//     //res.statusCode=200
+//     //res.setHeader('content-type' , 'text/plain')
+//     //res.end('HELLO , NODE JS')
+// //});
+// //server.listen(3000,() => {
+//     //console.log('Server running at http://localhost:3000/');
+// //});
 
-//const directory = '/user/local'
-//const fileName = 'example.txt'
-//const fullpath = path.join(directory, fileName)
-//console.log(fullpath);
+// //const directory = '/user/local'
+// //const fileName = 'example.txt'
+// //const fullpath = path.join(directory, fileName)
+// //console.log(fullpath);
 
-//console.log('platform:', os.platform());
-//console.log('architecture:', os.arch());
-//console.log('total memory:', os.totalmem());
-//console.log('free memory:', os.freemem());
+// //console.log('platform:', os.platform());
+// //console.log('architecture:', os.arch());
+// //console.log('total memory:', os.totalmem());
+// //console.log('free memory:', os.freemem());
 
-//const myUrl=new URL('http://example.com:8080/path/name?query=hello#hash')
-//console.log('host' ,myUrl.host);
-//console.log('pathname' ,myUrl.pathname);
-//console.log('search params' ,myUrl.searchParams.get('query'));
+// //const myUrl=new URL('http://example.com:8080/path/name?query=hello#hash')
+// //console.log('host' ,myUrl.host);
+// //console.log('pathname' ,myUrl.pathname);
+// //console.log('search params' ,myUrl.searchParams.get('query'));
  
-//const hash=crypto.createHash('sha256')//type of the hash
-//hash.update('Hello, NodeJs')//the message to update
-//console.log(hash.digest('hex'))//to finalize
+// //const hash=crypto.createHash('sha256')//type of the hash
+// //hash.update('Hello, NodeJs')//the message to update
+// //console.log(hash.digest('hex'))//to finalize
 
-//const message= sayHello('DEVELOPER')
-//console.log(message);
-//console.log(math.add (4,9));
-//console.log(math.multiply(4,7))
-
-
-  //onst numbers = [1, 2, 3, 4, 5];
-  // const reversed = lodash.reverse(numbers);
-  //console.log(reversed);
+// //const message= sayHello('DEVELOPER')
+// //console.log(message);
+// //console.log(math.add (4,9));
+// //console.log(math.multiply(4,7))
 
 
-const readablestream =fs.createReadStream('example.txt', {encoding: 'utf8'})
-readablestream.on('data' ,(chunk) => {
-  console.log(chunk);//to readthe file in chunks
-})
+//   //onst numbers = [1, 2, 3, 4, 5];
+//   // const reversed = lodash.reverse(numbers);
+//   //console.log(reversed);
 
-readablestream.on('end', () => {
-  console.log('Finished reading file');//to execute the command
-});
 
-readablestream.on('error', (err) => {
-  console.error('Error', err);
-});
+// const readablestream =fs.createReadStream('example.txt', {encoding: 'utf8'})
+// readablestream.on('data' ,(chunk) => {
+//   console.log(chunk);//to readthe file in chunks
+// })
+
+// readablestream.on('end', () => {
+//   console.log('Finished reading file');//to execute the command
+// });
+
+// readablestream.on('error', (err) => {
+//   console.error('Error', err);
+// });
+
+
+ WritableStream=fs.createWriteStream('output2.txt' )
+ WritableStream.write('I love node .js');
+ WritableStream.write('Hello Node.js');
+ WritableStream.end();
+
+ WritableStream.on('finish', () => {
+   console.log('Finished writing file');
+ });
