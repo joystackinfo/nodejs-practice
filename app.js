837c5c97ca089ac57bb79a3e41f37137cd0383a9
joystackinfo <joynelsoninfo@gmail.com>
2025-08-18 22:01:58 +0100

    removing and handling error

--- app.js
+++ app.js
@@ -191,8 +191,20 @@ const emitter = new EventEmitter()
     console.log('an event has occured in test 1');
   })
 
-  emitter.on('test1', ()=>{
-    console.log('an event has occured in test 2');
-  })
 
-  emitter.emit('test1');
+
+  emitter.on('error', (err) => {
+    console.error('Error event emitted:', err.message);
+  });
+
+
+  try {
+emitter.emit('test1');
+  emitter.removeListener('test1');
+  emitter.emit('test1'); // this will not log anything because the listener has been removed
+    
+  } catch (error) {
+    emitter.emit('error', error) // emit an error event if any error occurs
+  }
+
+  
