9ddb05a7a70116df6395434cefd32f72748752ad
joystackinfo <joynelsoninfo@gmail.com>
2025-08-18 14:25:00 +0100

    HANDLING THE POST METHOD

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const http = require('http'); // to get the http module
  
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/') {
-        res.writeHead(200, { 'content-type': 'text/plain' });
-        res.end('Welcome to the home page.');
+    if (req.method === 'POST' && req.url === '/submit') { // to check if the method is post and url is submit
+           let body = ''; // to store the incoming data
+           req.on('data', chunk => {
+               body += chunk.toString(); // convert it to string
+           });
+           req.on('end', () => {
+               console.log('Received data:', body); // to log the received data
+               res.writeHead(200, { 'content-type': 'application/json' }); // to set the response header
+               res.end(JSON.stringify({ message: 'Data received' }));  // to send the response
+           });
     } else {
         res.writeHead(404, { 'content-type': 'text/plain' });
-        res.end('Homepage not found');
+        res.end('route not found');
     }
 })
 const PORT = 3000;
