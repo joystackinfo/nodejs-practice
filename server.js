4b8332682c0b5a403b9d0e8b2eee972a1b29fb8d
joystackinfo <joynelsoninfo@gmail.com>
2025-08-18 13:54:16 +0100

    handling get request

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const http = require('http'); // to get the http module
-
-const server =http.createServer((req,res) => [ // create the server
-
-])
-
-const PORT=3000
+ 
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'content-type': 'text/plain' });
+        res.end('Welcome to the home page.');
+    } else {
+        res.writeHead(404, { 'content-type': 'text/plain' });
+        res.end('Homepage not found');
+    }
+})
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`) //to indicate success
 })
