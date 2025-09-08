58f9f704bbf6831870c94f10c18ed88ed2eefa37
joystackinfo <joynelsoninfo@gmail.com>
2025-09-08 14:37:16 +0100

    tested EVENTS

--- app.js
+++ app.js
@@ -185,42 +185,42 @@
 //        console.log('is directory:', stats.isDirectory());
 //     });
 
- fs.watch('./', (eventType, filename) => {
-     console.log(`event: ${eventType}`);
-    if (filename) {
-         console.log(`Filename: ${filename}`);
-    }
- });
+//  fs.watch('./', (eventType, filename) => {
+//      console.log(`event: ${eventType}`);
+//     if (filename) {
+//          console.log(`Filename: ${filename}`);
+//     }
+//  });
 
 
 
 
-// const EventEmitter = require('events');
+ const EventEmitter = require('events');
 
-// const emitter = new EventEmitter() 
-//   //registering a event listener
+const emitter = new EventEmitter() 
+  //registering a event listener
 
-//    emitter.on('test1', ()=>{
-//     console.log('an event has occured in test 1');
-//   })
+    emitter.on('test1', ()=>{
+     console.log('an event has occured in test 1');
+   })
 
-//   emitter.on('test1', ()=>{
-//     console.log('an event has occured in test 2');
-//   })
+   emitter.on('test1', ()=>{
+     console.log('an event has occured in test 2');
+  })
 
-//   emitter.emit('test1');
+   emitter.emit('test1');
 
-//   emitter.on('error', (err) => {
-//     console.error('Error event emitted:', err.message);
-//   });
+  emitter.on('error', (err) => {
+    console.error('Error event emitted:', err.message);
+  });
 
 
-//   try {
-// emitter.emit('test1');
-//   emitter.removeListener('test1');
-//   emitter.emit('test1'); // this will not log anything because the listener has been removed
+  try {
+ emitter.emit('test1');
+   emitter.removeListener('test1');
+   emitter.emit('test1'); // this will not log anything because the listener has been removed
     
-//   } catch (error) {
-//     emitter.emit('error', error) // emit an error event if any error occurs
-//   }
+   } catch (error) {
+     emitter.emit('error', error) // emit an error event if any error occurs
+  }
 
